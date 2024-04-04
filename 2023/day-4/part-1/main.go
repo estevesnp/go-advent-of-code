c59fa3c72287506d5339365090543e03bb97a4b9
esteves <nuno.pinto@live.com>
2024-04-04 14:21:03 +0100

    day 4 part 1

---
+++ 2023/day-4/part-1/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	input, err := os.Open("../input.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer input.Close()
+
+	fmt.Println("RESULT:", solution(input))
+	//fmt.Println(parseCard("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53"))
+}
+
+func solution(input *os.File) int {
+	var count int
+
+	scanner := bufio.NewScanner(input)
+	for scanner.Scan() {
+		count += parseCard(scanner.Text())
+	}
+
+	return count
+}
+
+func parseCard(c string) int {
+
+	var winNums, pNums []int
+
+	sepNums := strings.Split(c[strings.Index(c, ":")+1:], "|")
+
+	winNums = getNums(strings.TrimSpace(sepNums[0]))
+	pNums = getNums(strings.TrimSpace(sepNums[1]))
+
+	count := 0
+
+	for _, p := range pNums {
+		for _, w := range winNums {
+			if p == w {
+
+				if count == 0 {
+					count++
+					break
+				}
+				count *= 2
+				break
+			}
+		}
+	}
+
+	return count
+}
+
+func getNums(s string) []int {
+	nums := []int{}
+	split := strings.Split(s, " ")
+
+	for _, n := range split {
+		num, err := strconv.Atoi(n)
+		if err != nil {
+			continue
+		}
+		nums = append(nums, num)
+	}
+
+	return nums
+}
